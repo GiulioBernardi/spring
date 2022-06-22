c97bfca3a391e6c3728b430f0dfc7c75e393fc01
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-06-22 18:14:31 -0300

    Configurei application.properties

--- src/main/java/com/giulio/controlefuncspring/ControleFuncSpringApplication.java
+++ src/main/java/com/giulio/controlefuncspring/ControleFuncSpringApplication.java
@@ -1,13 +1,31 @@
 package com.giulio.controlefuncspring;
 
+import com.giulio.controlefuncspring.model.Funcionario;
+import com.giulio.controlefuncspring.repository.FuncionarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ControleFuncSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ControleFuncSpringApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(FuncionarioRepository funcionarioRepository){
+        return args -> {
+            funcionarioRepository.deleteAll();
+
+            Funcionario f = new Funcionario();
+
+            f.setNome("Carlos");
+            f.setIdade(20);
+
+            funcionarioRepository.save(f);
+        };
     }
 
 }
